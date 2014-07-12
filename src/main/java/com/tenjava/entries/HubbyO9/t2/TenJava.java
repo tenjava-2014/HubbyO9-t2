e1afafcd3436a813a9add0e3248752b1c8c47c38
hubertus09 <hubbyissoawesome@gmail.com>
2014-07-12 10:45:11 +0100

    First Commit <3

--- src/main/java/com/tenjava/entries/HubbyO9/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/HubbyO9/t2/TenJava.java
@@ -2,4 +2,15 @@ package com.tenjava.entries.HubbyO9.t2;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable(){
+
+	}
+}
